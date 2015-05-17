f6b9ae309528904b0c174c60133676a437a7db54
Joe Reeve <joe@wigwamm.com>
2015-05-17 09:29:50 +0100

    level 2 template and level 3

--- games/spriteVsGroup/app.js
+++ games/spriteVsGroup/app.js
@@ -14,4 +14,26 @@ app.get('/', function(req, res) {
   });
 });
 
+app.get('/two', function(req, res) {
+  Load('views/two.html').then(function(Component) {
+    var ractive = new Component({
+      data: {}
+    });
+    res.send(ractive.toHTML());
+  }).catch(function(a, b) {
+    console.log(a, b)
+  });
+});
+
+app.get('/three', function(req, res) {
+  Load('views/three.html').then(function(Component) {
+    var ractive = new Component({
+      data: {}
+    });
+    res.send(ractive.toHTML());
+  }).catch(function(a, b) {
+    console.log(a, b)
+  });
+});
+
 app.listen(1337);
