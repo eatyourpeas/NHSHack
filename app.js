fbd26a0232a851f0578f2bb44b3d371be827cef5
Mike Monteith <mike@movement.co.uk>
2015-05-17 10:01:24 +0100

    GET and POST scores

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var app = require('express')();
+var bodyParser = require('body-parser');
 var Load = require('ractive-load');
+var mongojs = require('mongojs');
+
+var db = mongojs('localhost', ['diabeaters']);
 
 app.use(require('express').static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.get('/', function(req, res) {
   Load('views/index.html').then(function(Component) {
@@ -12,6 +17,37 @@ app.get('/', function(req, res) {
   }).catch(function(a, b) {
     console.log(a, b)
   });
+});
+
+app.get('/scores/', function(req, res){
+	console.log("scores");
+	var limit = req.query.limit || 10;
+
+	//get top scores
+	db.collection('scores').find({}).limit(limit, function(err, result){
+		console.log(err, result);
+		if(err) return res.sendStatus(500);
+
+		res.send(result);
+	});
+});
+
+app.post('/scores/', function(req, res){
+	//score record = name + score
+	var score = req.body.score;
+	var name = req.body.name;
+
+	//TODO: do some validation here!
+
+	db.collection('scores').insert({
+		score: score,
+		name: name,
+	}, function(err){
+		console.log(err);
+		if(err) return res.sendStatus(500);
+
+		res.sendStatus(201);
+	});
 });
 
 app.listen(1337);
